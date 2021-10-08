f95d5e0e8d7ce417b852dd1f843d5558f385d7c5
sunny01912 <sunnyraj01912@gmail.com>
2021-10-08 20:47:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModel = function () {
+  console.log('btn clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  modal.classList.add('open');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModel);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
